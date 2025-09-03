bfde4350958481c316d7e8973811cfd9c99ce20c
Iqbolshoh Ilhomjonov <148738947+Iqbolshoh@users.noreply.github.com>
2025-09-03 19:15:59 +0500

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/storage': 'http://localhost:3001',
+      '/students': 'http://localhost:3001',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
